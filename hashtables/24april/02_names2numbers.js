b4125f04f269acf61ea62a896dde928198d1b5c9
Gonçalo Morais <gnclmorais@gmail.com>
2015-04-24 19:27:18 -0400

    Documentation.

--- hashtables/24april/02_names2numbers.js
+++ hashtables/24april/02_names2numbers.js
@@ -3,10 +3,14 @@
  */
 module.exports = (function names2numbers () {
   return function (word) {
+    // Generates numbers like 10 or 1000000, providing the number of 0s.
     function bigNumber(numZeros) {
       return Number('1' + Array(numZeros + 1).join('0'));
     }
 
+    // Splits the words, reverses it, and sums every character code
+    // multiplied by a number with the same order or greatness as
+    // the charecter's index.
     return word.split('').reverse().reduce(function (acc, cur, idx) {
       return acc + cur.charCodeAt(0) * bigNumber(idx);
     }, 0);
